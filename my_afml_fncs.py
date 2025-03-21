66f06cc68a495272f1a542588d220b880d957a7d
slothkins <nicolas.atkins@me.com>
2025-03-21 22:23:27 +0000

    5.5

--- my_afml_fncs.py
+++ my_afml_fncs.py
@@ -439,17 +439,24 @@ def getWeights(d, size):
     w = [1.]
     for k in range(1, size):
         w_ = -w[-1] / k * (d - k + 1)
-    w.append(w_)
+        w.append(w_)
     w = np.array(w[::-1]).reshape(-1, 1)
     return w
 
+def getWeights_FFD(d,thres):
+    w,k=[1.],1
+    while True:
+        w_=-w[-1]/k*(d-k+1)
+        if abs(w_)<thres:break
+        w.append(w_);k+=1
+    return np.array(w[::-1]).reshape(-1,1)
 
 def plotWeights(dRange, nPlots, size):
     w = pd.DataFrame()
     for d in np.linspace(dRange[0], dRange[1], nPlots):
         w_ = getWeights(d, size=size)
-    w_ = pd.DataFrame(w_, index=range(w_.shape[0])[::-1], columns=[d])
-    w = w.join(w_, how='outer')
+        w_ = pd.DataFrame(w_, index=range(w_.shape[0])[::-1], columns=[d])
+        w = w.join(w_, how='outer')
     ax = w.plot()
     ax.legend(loc='upper left');
     mpl.show()
@@ -471,7 +478,7 @@ def fracDiff(series, d, thres=.01):
     # 3) Apply weights to values
     df = {}
     for name in series.columns:
-        seriesF, df_ = series[[name]].fillna(method='ffill').dropna(), pd.Series()
+        seriesF, df_ = series[[name]].ffill().dropna(), pd.Series()
         for iloc in range(skip, seriesF.shape[0]):
             loc = seriesF.index[iloc]
             if not np.isfinite(series.loc[loc, name]): continue  # exclude NAs
@@ -480,3 +487,16 @@ def fracDiff(series, d, thres=.01):
     df = pd.concat(df, axis=1)
     return df
 
+def fracDiff_FFD(series,d,thres=1e-5):
+    # Constant width window (new solution)
+    w=getWeights_FFD(d,thres)
+    width,df=len(w)-1,{}
+    for name in series.columns:
+        seriesF, df_ = series[[name]].ffill().dropna(), pd.Series()
+        for iloc1 in range(width, seriesF.shape[0]):
+            loc0, loc1 = seriesF.index[iloc1 - width], seriesF.index[iloc1]
+            if not np.isfinite(series.loc[loc1, name]): continue  # exclude NAs
+            df_[loc1] = np.dot(w.T, seriesF.loc[loc0:loc1])[0, 0]
+        df[name] = df_.copy(deep=True)
+    df = pd.concat(df, axis=1)
+    return df
